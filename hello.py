5d13f776e3ea34a623f0d9d07995c6fa25bb1b92
Balhau <balhau@balhau.net>
2013-10-12 01:26:25 +0000

    Added files to the available list of video files

--- hello.py
+++ hello.py
@@ -43,7 +43,7 @@ def print_post():
 
 @app.route('/listdir',methods=['POST'])
 def list_dir():
-	extensions=['mp4','mkv']
+	extensions=['mp4','mkv','avi','flv']
 	path=request.form['path']
 	if path :
 		jsData=[f for f in listdir(path) if f[-3:] in extensions]
