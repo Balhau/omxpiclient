4357d3b42eef79853a92de5065d9afc3930f192a
Balhau <balhau@balhau.net>
2013-10-15 23:01:55 +0000

    Start to convert calls into subprocesses. Piping stdin to control omxplayer. Preparing controllers to up/down sound. Tests with omxplayyt youtube utility

--- hello.py
+++ hello.py
@@ -1,17 +1,60 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-import os
-import json
+import subprocess,shlex,json,os,signal
 from os import listdir
 from os.path import isfile, join
 
 
 app=Flask(__name__)
 
+omxProcess=None
+
 @app.route('/')
-def hello_world():
+def index():
 	return render_template('index.html')
+
+@app.route('/youtube')
+def youtube():
+	return render_template('youtube.html')
+
+@app.route('/teste')
+def teste():
+	global omxProcess
+	cmdLine='omxplayer /home/pi/def20ec.mp4'
+	cmdLine='omxplayyt "http://www.youtube.com/watch?v=5L6TejdJXCc"'
+	args=shlex.split(cmdLine)
+	omxProcess=subprocess.Popen(args,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+	return '{"sound_up":true}'
+
+@app.route('/killprocc')
+def killprocc():
+	global omxProcess
+	print omxProcess
+	if omxProcess != None:
+		print omxProcess.pid
+		os.system('killall omxplayer.bin')
+		omxProcess=None
+		return '{"status":true}'
+	return '{"status":false}'
+
+@app.route('/sound_up')
+def up_sound():
+	global omxProcess
+	if omxProcess!=None:
+		omxProcess.stdin.write("+")
+		return '{"command":true}'
+	return '{"status":false}'
+
+@app.route('/sound_down')
+def down_sound():
+	global omxProcess
+	if omxProcess!=None:
+		omxProcess.stdin.write("-")
+		return '{"status":true}'
+	return '{"status":true}'
+
+
 
 @app.route('/killomx')
 def kill_omxplayer():
@@ -27,7 +70,6 @@ def print_get():
 
 def sh_quote(s):
     si=s.replace(" ",'\\ ')
-    
     return si
 
 @app.route('/play',methods=['POST'])
